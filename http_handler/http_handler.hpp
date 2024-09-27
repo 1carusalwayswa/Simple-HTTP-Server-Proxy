2f6a49ad9dc5f76f7037b166b3f0a49d0641ddbd
yang <theonlyprince@126.com>
2024-09-27 17:42:45 +0200

    Update http_handler.

--- http_handler/http_handler.hpp
+++ http_handler/http_handler.hpp
@@ -1,11 +1,13 @@
 #include <string>
 #include <sstream>
+#include <regex>
 
 class HttpHandler {
 public:
     HttpHandler() = default;
 
     void SetHttpHandler(std::string msg_, int port_) {
+        ori_msg = msg_;
         msg = msg_;
         port = port_;
         handler_type = "";
@@ -24,61 +26,96 @@ public:
         }
     }
 
-    std::string GetHost() {
+    std::string& GetHost() {
         return host;
     }
 
-    std::string GetPath() {
+    std::string& GetPath() {
         return path;
     }
 
-    std::string GetMethod() {
+    std::string& GetMethod() {
         return method;
     }
 
-    std::string GetHttpVersion() {
+    std::string& GetHttpVersion() {
         return http_version;
     }
 
-    std::string GetStatusCode() {
+    std::string& GetStatusCode() {
         return status_code;
     }
 
-    std::string GetStatusPhrase() {
+    std::string& GetStatusPhrase() {
         return status_phrase;
     }
 
-    std::string GetContentLength() {
+    std::string& GetContentLength() {
         return content_length;
     }
 
-    std::string GetContentType() {
+    std::string& GetContentType() {
         return content_type;
     }
 
-    std::string GetBody() {
+    std::string& GetBody() {
         // std::cout << "-----------------\n";
         // std::cout << body;
         // std::cout << "!!!!!!!!!!!!!!!!!\n";
         return body;
     }
 
-    std::string GetHandlerType() {
+    const std::string& GetOriMsg() {
+        return ori_msg;
+    }
+
+    std::string& GetHandlerType() {
         return handler_type;
     }
 
-    int GetPort() {
+    std::string& GetHttpConnection() {
+        return http_connection;
+    }
+
+    int& GetPort() {
         return port;
     }
 
-    void SetPort(int port_) {
-        port = port_;
+    std::string GetRequest() {
+        if (handler_type == "request") {
+            std::string tmp_str = method + " " + path + " " + http_version;
+            msg = ReplaceFirstLine(msg, tmp_str);
+            msg = ReplaceField(msg, "Host", host);
+            return msg;
+        } else {
+            return "";
+        }
+    }
+
+    std::string GetResponse() {
+        if (handler_type == "response") {
+            msg = ReplaceField(ori_msg, "Content-Length", content_length);
+            msg = ReplaceField(msg, "Content-Type", content_type);
+            msg = ReplaceField(msg, "Connection", http_connection);
+            msg = ReplaceField(msg, "body", body);
+            return msg;
+        } else {
+            return "";
+        }
+    }
+
+    void ResetMsg() {
+        msg = ori_msg;
     }
 
 private:
     // original msg
+    std::string ori_msg;
     std::string msg;
+
+    // common fields
     std::string http_version;
+    std::string http_connection;
 
     // Request
     std::string host;
@@ -91,9 +128,25 @@ private:
     std::string content_length;
     std::string content_type;
     std::string body;
+    bool is_encoding;
 
     std::string handler_type;
     int port;
+
+    std::string ReplaceFirstLine(const std::string& msg, const std::string& new_first_line) {
+        size_t pos = msg.find("\r\n");
+        if (pos != std::string::npos) {
+            return new_first_line + msg.substr(pos);
+        } else {
+            return new_first_line;
+        }
+    }
+
+    std::string ReplaceField(const std::string& http_request, const std::string& field_name, const std::string& new_value) {
+        std::regex field_regex(field_name + ":\\s*[^\\r\\n]*");
+        std::string replacement = field_name + ": " + new_value;
+        return std::regex_replace(http_request, field_regex, replacement);
+    }
 
     void ParseRequest(std::string msg) {
        // Parse the msg
@@ -128,12 +181,17 @@ private:
 
        // Get the body
        while (std::getline(iss, line)) {
-            if (line.find("Content-Length:") != std::string::npos) {
+            if (line.find("Connection:") != std::string::npos) {
+                std::istringstream iss_line(line);
+                iss_line >> tmp >> http_connection;
+            } else if (line.find("Content-Endcoding") != std::string::npos) {
+                is_encoding = true;
+            } else if (line.find("Content-Length:") != std::string::npos) {
                 std::istringstream iss_line(line);
                 iss_line >> tmp >> content_length;
-            } else if (line.find("Content-Type:" != std::string::nops)) {
+            } else if (line.find("Content-Type:") != std::string::npos) {
                 std::istringstream iss_line(line);
-                iss_line >> tmp >> content_length;
+                iss_line >> tmp >> content_type;
             } else if (line.find("<html>") != std::string::npos) {
                 body += line;
                 if (line.find("</html>") != std::string::npos) { 
